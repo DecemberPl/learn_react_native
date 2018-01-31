8b54188d72db504cbb6582ff0f69833f58e45fd3
PhyuEi <december.pl1991@gmail.com>
2018-01-31 09:24:35 +0630

    edited to change app name

--- android/app/src/main/java/com/awesomeproject_1/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject_1/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "AwesomeProject_1";
+        return "learn_react_native";
     }
 }
