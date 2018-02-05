cd1c266ddf65f914bcfa4ecee7d4567ed64afc06
PhyuEi <december.pl1991@gmail.com>
2018-02-05 09:07:40 +0630

    edited to add navigation

--- App.js
+++ App.js
@@ -4,7 +4,10 @@
  * @flow
  */
 import React, { Component } from 'react';
-import axios from "axios";
+import { NavigationActions } from "react-navigation";
+import axios from 'axios';
+import {StackNavigator} from 'react-navigation';
+//import HomeScreen from './src/navigation/screen/HomeScreen';
 import {
   Platform,
   StyleSheet,
@@ -22,8 +25,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-class App extends Component<{}> {
+class App extends React.Component {
 
+  static navigationOptions = {
+    title: 'Welcome'
+  };
   constructor(props) {
     super(props);
     this.state = {isShowingText: true};
@@ -45,30 +51,86 @@ class App extends Component<{}> {
   }
 }
 
-export default class LotsOfGreetings extends Component {
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={{alignItems: 'center'}}>
-        <App name='Rexxarr' />
-        <App name='Jainaaaaa' />
-        <App name='Valeera' />
-        <Button  onPress={this.onPressLearnMore.bind(this)}
-      title="Learn More"/>
+    <View>
+    
+    <Button title="Go to Chat Screen"
+    onPress={() => navigate('ChatScreen')}
+  />
+  </View>
+    );
+  }
+}
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Button title="Go to Home Screen"
+         onPress={() => navigate('Home')}
+        />
       </View>
-      
+    );
+  }
+}
+
+ const RootNavigator = StackNavigator({
+  Home: { screen: HomeScreen },
+  //LotsOfGreetings:{screen: LotsOfGreetings},
+  ChatScreen:{screen: ChatScreen}},
+  {
+    initialRouteName: 'ChatScreen',
+  });
+
+export default class LotsOfGreetings extends Component {
+  constructor(props) {
+  super(props);
+  this.state = { navigation: this.props.navigation } ;
+  }
+
+
+  render() {
+    //const { navigate } = this.props.navigation;
+    //<Button title="Go to Jane's profile" onPress={() =>this.state.navigation.navigate('Home')}/>
+   //<Button title="Go to Jane's profile" onPress={() => navigation('Home')}/>
+    return (
+     // <View style={{alignItems: 'center'}}>
+       // <App name='Rexxarr' />
+       // <App name='Jainaaaaa' />
+      //  <App name='Valeera' />
+       // <Button  onPress={this.onPressLearnMore.bind(this)}  title="Learn More"/>
+      //  <Button  onPress={this.onPressLearnMore.bind(this)}  title="To App 1"/>
+      //  <Button title="Go to next" onPress={this.onPressLearnMore.bind(this)}/>
+      // <Button title="Go to Jane's profile" onPress={()=>this.state.navigation.navigate('Home')}/>
+     // </View>
+     
+       <RootNavigator />
+    
     );
 
   }
 
   onPressLearnMore(){
- 
-    Alert.alert("Function Without Argument");
+    //const { navigate } = this.props.navigation;
+    //Alert.alert("Function Without Argument");
     axios.get("http://192.168.4.151:8082/Qarah5_WebApp/api/user/termandcondition")
     .then(res => {
       const posts = res.data;
-      Alert.alert(posts.TermandConditions);
+     // Alert.alert(posts.TermandConditions);
       this.setState({ posts });
     });
+    Alert.alert(this.state.navigation);
     
   }
  
